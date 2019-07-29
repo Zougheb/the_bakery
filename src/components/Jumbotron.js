96df6f1ec8dfaa7ce8e6d3236bcc13aa2ff5c8b3
Mahmoud Zougheb <m.zougheb@gmail.com>
2019-07-29 00:17:34 -0400

    fixing spelling errors

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,7 +5,7 @@ export default function Jumbotron(props) {
         <div className="jumbotron ">
             
                 <h1 className="display-4">Welcome To The Bakery... :)</h1>
-                <p  className="lead">Try our freshly homemade baked sweets & savory goodies, Everyday for your breakfast, lunch and dinner...</p>
+                <p  className="lead">Try our freshly homemade baked sweet & savory goodies. Everyday for your breakfast, lunch and dinner!</p>
         </div>
     );
 }
